f7fa00131abd7d2e1f16565f42dc2c28b33e614b
daniel rocha <to.danielrp@gmail.com>
2018-01-24 09:59:18 -0400

    initial industrial security spring boot app

---
+++ src/main/java/com/dharbor/industrialsecurity/IndustrialSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.dharbor.industrialsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialSecurityApplication.class, args);
+	}
+}
